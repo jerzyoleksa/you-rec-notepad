435076f580c37438fa79a837fd8b16e0aeec8c93
jerzyoleksa <jerzy.oleksa@gmail.com>
2021-03-25 10:32:39 +0100

    cleaning code

--- src/components/Current.js
+++ src/components/Current.js
@@ -3,40 +3,27 @@ import axios from 'axios';
 import urlFor from '../helpers/urlFor';
 import NoteCard from './NoteCard';
 
-// function updateDisplayNote(text){
-//   this.setState({value: text})
-// }
 
 class Current extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      notesX: [],
-      displayNote: {},
-      value: ''
+      value: this.props.prop1.content
     };
-
- 
-
     this.handleChange = this.handleChange.bind(this);
-    //updateDisplayNote = updateDisplayNote.bind(this);
-
   }
 
  
 
   getNotes = () => {
-
-
     //use https not http! to avoid problems with redirect/cors
     axios.get('https://frengly.com/ai/notes')
     .then((res) => {
     
       let list = res.data;
       let lastNote = list[list.length-1];
-      console.log("lastNote::"+lastNote);
-      this.setState({notesX: list, displayNote: lastNote, value: lastNote[2]});
+      this.setState({value: lastNote[2]});
     
     })
     .catch((err) => console.log("Error!!!",err) );
@@ -45,17 +32,10 @@ class Current extends Component {
   //TODO: call parent method to update prent state of selectedNote
   handleChange(event) {
     this.setState({value: event.target.value});
-    console.log("textarea changed to:"+this.state.value);
     this.props.updateNoteContent(event.target.value);
   }
 
-  componentWillMount() {
-    this.state.value = this.props.prop1.content;
-    //this.getNotes(); //this sets notesX in state
-  }
-
-
-
+  //update state of Current based on prop1 that holds selectedNote in parent
   componentWillReceiveProps(nextProps) {
     // This will erase any local state updates!
     // Do not do this.
@@ -70,26 +50,14 @@ class Current extends Component {
   render() {
   
     return (
-
-    
+  
       <div class="note-textarea-container">
         <div>parent selectedNote:{this.props.prop1.content}</div>
         {/* {this.state.displayNote.title} {this.state.displayNote.content}<br></br> */}
         
         <textarea value={this.state.value} onChange={this.handleChange} ref={(input) => { this.nameInput = input; }} />
-   
       
       </div>
-
-      
-        
-            
-      
-      
-      
-
-    
-
 
     );
   }
