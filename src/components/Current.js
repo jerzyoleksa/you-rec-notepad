a318c9e036d86ea2787a7d23782d21955914e082
jerzyoleksa <jerzy.oleksa@gmail.com>
2021-03-26 11:33:32 +0100

    detect when user stopped typing and then update note on server

--- src/components/Current.js
+++ src/components/Current.js
@@ -9,30 +9,42 @@ class Current extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.prop1.content
+      value: this.props.prop1.content,
+      typing: false,
+      typingTimeout: 0
     };
     this.handleChange = this.handleChange.bind(this);
+    
   }
 
- 
 
-  getNotes = () => {
-    //use https not http! to avoid problems with redirect/cors
-    axios.get('https://frengly.com/ai/notes')
+  updateNote = () => {
+
+    let noteToUpdate = this.props.prop1;
+    console.log('updating:'+noteToUpdate);
+
+    axios.put('https://frengly.com/ai/notes', noteToUpdate)
     .then((res) => {
     
-      let list = res.data;
-      let lastNote = list[list.length-1];
-      this.setState({value: lastNote[2]});
-    
     })
-    .catch((err) => console.log("Error!!!",err) );
+    .catch((err) => console.log("Error updating!!!",err) );
   }
 
   //TODO: call parent method to update prent state of selectedNote
   handleChange(event) {
     this.setState({value: event.target.value});
     this.props.updateNoteContent(event.target.value);
+
+    if (this.state.typingTimeout) {
+      clearTimeout(this.state.typingTimeout);
+    }
+    
+    this.state.typing = false;
+    this.state.typingTimeout = setTimeout(() => {
+      this.updateNote();  
+    }, 3000);
+
+
   }
 
   //update state of Current based on prop1 that holds selectedNote in parent
