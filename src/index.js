d053b83952ac1c0a119bc7fb5cf18c20d7ab962b
jerzyoleksa <jerzy.oleksa@gmail.com>
2022-09-11 10:14:58 +0200

    simplified AppX

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import AppX from './AppX';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppX />, document.getElementById('root'));
 //registerServiceWorker();
