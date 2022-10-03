f94dadcf63dbca0c8b03ce6a83aac3d28d03982b
jerzyoleksa <jerzy.oleksa@gmail.com>
2022-10-03 17:26:18 +0200

    add index.js to repo

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppX from './AppX';
+import AppZ from './AppZ';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<AppZ />, document.getElementById('root'));
+//registerServiceWorker(); //uncomment and you might get CORS issues (503) LOCALLY
