5dd77d1361d10c8ef1d59374a2160979de4b41c5
jerzyoleksa <jerzy.oleksa@gmail.com>
2022-10-24 20:22:47 +0200

    fixed AES encryption for Sec notes

--- src/components/EncryptAES.js
+++ src/components/EncryptAES.js
@@ -1,48 +1,55 @@
-import React, { useState } from 'react';
-import axios from 'axios';
 import CryptoJS from "crypto-js";
 
+let key = 'secret#456!23key' //(16 Byte key for 128 Bit AES)
 
-let key = 'FE7A45426AFF5D14E52897E134F5CC33'
-
-const encodePass = (password) =>{
-  const encodedWord = CryptoJS.enc.Utf8.parse(password); // encodedWord Array object
-  const encoded = CryptoJS.enc.Base64.stringify(encodedWord); // string: 'NzUzMjI1NDE='
-  return encoded;
-};
-
-const decryptWithAES = (ciphertext, myPass) => {
-  //const passphrase = '123';
-  var encryptedBase64Key = encodePass(myPass);
-  var parsedBase64Key = CryptoJS.enc.Base64.parse(encryptedBase64Key);
-
-  const bytes = CryptoJS.AES.decrypt(ciphertext, key, {
+const decryptWithAES = (ciphertext) => {
+  const bytes = CryptoJS.AES.decrypt(ciphertext, CryptoJS.enc.Utf8.parse(key), {
     mode: CryptoJS.mode.ECB,
     padding: CryptoJS.pad.Pkcs7
   });
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
 
+const encryptTextWithAES = (plaintText) => {
+  const encryptedData = CryptoJS.AES.encrypt(plaintText, CryptoJS.enc.Utf8.parse(key), {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+  });
+  return encryptedData.toString();    
+}
+export {encryptTextWithAES, decryptWithAES}
+
+const aesEncrypt = (data) => {
+  const key = '6fa979f20126cb08aa645a8f495f6d85'
+  const iv = 'I8zyA4lVhMCaJ5Kg'
   
-  const originalText = bytes.toString(CryptoJS.enc.Utf8);
-  return originalText;
+  const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+      iv: CryptoJS.enc.Utf8.parse(iv), // parse the IV 
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+  })
+  
+  // e.g. B6AeMHPHkEe7/KHsZ6TW/Q==
+  return cipher.toString()
 }
 
-const encryptTextWithAES = (plaintText, myPass) => {
-    //var myPass="0123456789123456" //must have 16 chars
-    var encryptedBase64Key = encodePass(myPass);
-    console.log('encryptedBase64Key::'+encryptedBase64Key);
-    var parsedBase64Key = CryptoJS.enc.Base64.parse(encryptedBase64Key);
-    console.log('parsedBase64Key::'+parsedBase64Key);
-    var encryptedData = null;
-    
 
-    // this is Base64-encoded encrypted data
-    encryptedData = CryptoJS.AES.encrypt(plaintText, key, {
-      mode: CryptoJS.mode.ECB,
-      padding: CryptoJS.pad.Pkcs7
-    });
+function getDecryptedCode() {
+  var key = CryptoJS.enc.Hex.parse(
+    "814591256d331af80bec0fa2bef1123e37e9f181f363af374787e24160275bce"
+  );
+  var iv = CryptoJS.enc.Hex.parse("825b1f7c5f5edd614e8a0a0fef3c9ecf");
+  var ciphertext = CryptoJS.enc.Base64.parse("07KxrSbGIoPCIYh0I16maw==");
+  var encryptedCP = CryptoJS.lib.CipherParams.create({
+    ciphertext: ciphertext,
+    formatter: CryptoJS.format.OpenSSL
+  });
+  var decryptedWA = CryptoJS.AES.decrypt(encryptedCP, key, {
+    iv: iv
+  });
+  var decryptedUtf8 = decryptedWA.toString(CryptoJS.enc.Utf8);
 
-    return encryptedData.toString();
-    
+  return decryptedUtf8;
 }
 
-export {encryptTextWithAES, encodePass, decryptWithAES}
+export {encryptTextWithAES, decryptWithAES}
